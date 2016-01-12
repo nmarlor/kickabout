1f2183748a987bfa33d272edef5cba0423d27cc8
nmarlor <nmarlor@MFB00424_NMARLO.myfuturesbright.local>
2016-01-12 09:38:32 +0000

    sprint1- commented out script

--- kickabout/src/main/resources/db_scripts/default_users.sql
+++ kickabout/src/main/resources/db_scripts/default_users.sql
@@ -6,6 +6,6 @@
 --	(id, address_line_1,  address_line_2, city, county, email, post_code, telephone)
 --VALUES (1, '1 MediaCityUk', 'Salford Quays', 'Salford', 'Greater Manchester', 'peel@mediacityuk.co.uk', 'M50 2NT', '0161 7777777');
 
-INSERT INTO account
-	(id, email, name, password, role)
-VALUES (3, 'peelaccount@hotmail.com', 'peel', 'password', 'ROLE_USER');
+--INSERT INTO account
+--	(id, email, name, password, role)
+--VALUES (3, 'peelaccount@hotmail.com', 'peel', 'password', 'ROLE_USER');
