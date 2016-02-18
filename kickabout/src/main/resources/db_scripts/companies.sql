ff6ddd78f8208995987d68fdca3737d82348c77f
Nick <nickmarlor@hotmail.com>
2016-02-18 15:41:07 +0000

    sprint3- companies scripts and css

---
+++ kickabout/src/main/resources/db_scripts/companies.sql
@@ -0,0 +1,8 @@
+INSERT INTO `companies` (`id`, `name`, `account_id`, `company_details_id`) VALUES
+	(1, 'Bury College', 2, NULL),
+	(2, 'Bury Council', 2, NULL),
+	(3, 'Play Football', 2, NULL),
+	(4, 'Power League', 2, NULL),
+	(5, 'Manchester City Council', 2, NULL),
+	(6, 'Salford Council', 2, NULL),
+	(7, 'Salford University', 2, NULL);
