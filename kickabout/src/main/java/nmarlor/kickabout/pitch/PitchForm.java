5758d779aba816e503989b2f8f27b5204c8306c6
Nick <nickmarlor@hotmail.com>
2016-03-08 14:05:19 +0000

    sprint4- change aspects of pitch availability page

---
+++ kickabout/src/main/java/nmarlor/kickabout/pitch/PitchForm.java
@@ -0,0 +1,24 @@
+package nmarlor.kickabout.pitch;
+
+public class PitchForm {
+
+	private String date;
+	private Long pitchId;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public Long getPitchId() {
+		return pitchId;
+	}
+
+	public void setPitchId(Long pitchId) {
+		this.pitchId = pitchId;
+	}
+	
+}
