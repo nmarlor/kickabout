8bcbc8b1fc0162f769d6df09aaec6f5839985281
Nick <nickmarlor@hotmail.com>
2016-03-15 15:09:24 +0000

    sprint5- created date picker for viewing a pitches booking schedule

--- kickabout/src/main/java/nmarlor/kickabout/pitch/PitchForm.java
+++ kickabout/src/main/java/nmarlor/kickabout/pitch/PitchForm.java
@@ -2,16 +2,7 @@ package nmarlor.kickabout.pitch;
 
 public class PitchForm {
 
-	private String date;
 	private Long pitchId;
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
 
 	public Long getPitchId() {
 		return pitchId;
